60771f580f25f51d568f5430f7ee87ac68b5da87
Sunil Mohan <sunilvm2017@email.iimcal.ac.in>
2022-12-09 23:33:15 +0530

    asdasd

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,5 +1,31 @@
 const Movie = require("../models/movie.model");
 
 module.exports.createMovie = async(req,res,next)=>{
-    const {title,year,poster,genres,imdbRating} = req.body;
+    const {title,year,poster,genres,imdbRating, createdBy} = req.body;
+    try{
+        if(!title|!year|!poster|!genres|!imdbRating){
+            return res.status(202).json({msg:"Please fill in all details"})
+        }
+
+        const checkMovie = await Movie.findOne({title:title,year:year});
+    if(checkMovie){
+        return res.status(202).json({msg:"Movie already exists"})
+    }
+    const movie = await Movie.create(req.body);
+
+    return res.status(200).json(movie)
+    }
+    catch (e) {
+    next(e);
 }
+}
+
+// module.exports.editMovie = async(req,res,next) =>{
+//     const {title,year,poster,genres,imdbRating, createdBy} = req.body;
+//     try{
+//         if(!title|!year|!poster|!genres|!imdbRating){
+//             return res.status(202).json({msg:"Please fill in all details"})
+//         }
+
+
+// }
--- models/user.model.js
+++ models/user.model.js
@@ -3,7 +3,7 @@ const UserSchema=new Schema({
     name:{type:String, required:true},
     email:{type:String,required:true,unique:true},
     password:{type:String, required:true},
-    admin:{type:String,required:true}
+    admin:{type:String,required:true, enum:["admin","normal"]}
 })
 const User=model("user",UserSchema)
 module.exports=User;
