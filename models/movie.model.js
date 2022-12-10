e0d9b9504396ee8ba726c7833f1b30cfe8a388a5
Sunil Mohan <sunilvm2017@email.iimcal.ac.in>
2022-12-11 01:19:26 +0530

    asdasd

--- models/movie.model.js
+++ models/movie.model.js
@@ -4,9 +4,11 @@ const MovieSchema=new Schema({
     title:{type:String, required:true},
     year:{type:Number,required:true},
     genres:{type:[String], required:true},
-    poster:{type:String,required:true},
+    posterurl:{type:String,required:true},
     imdbRating: {type:Number, required:true, min: 1, max:10},
-    createdBy:{type:String, required:true}
+    createdBy:{type:String, required:true},
+    actors:{type:[String], required:true}
+    
     // contentRating:{}
 })
 MovieSchema.index({title:1,year:1},{unique:true})
