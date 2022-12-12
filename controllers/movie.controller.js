9295cd22e54fa1c14da570a21a50033fa2645c47
Sunil Mohan <sunilvm2017@email.iimcal.ac.in>
2022-12-12 21:09:58 +0530

    Asdsad

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -20,7 +20,7 @@ module.exports.findMovies = async(req,res,next) =>{
     
        Movie.find((err,docs) => {
 if(err) {
-return res.status(300).json(err)
+return res.status(400).json(err)
 // console.log(err)
 } else {
 return res.status(200).json(docs)
@@ -44,6 +44,6 @@ module.exports.myMovies = async(req,res,next)=>{
     // console.log(req.body);
     Movie.find({createdBy:userEmail})
         .then((docs)=> res.status(200).json(docs))
-        .catch((err)=>res.status(300).json(err))
+        .catch((err)=>res.status(400).json(err))
 
 }
