aaaf4b21121425906103e34c270b1bfa781318aa
Sunil Mohan <sunilvm2017@email.iimcal.ac.in>
2022-12-14 17:54:13 +0530

    asdasd

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app=express();
 
 // Curb Cores Error by adding a header here
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://eclectic-jelly-fdf977.netlify.app/");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
