2a1a83edfb375d26b99ac2c017d5b4ec3015a6ae
Sunil Mohan <sunilvm2017@email.iimcal.ac.in>
2022-12-14 19:36:50 +0530

    asdasd

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ require('dotenv').config()
 const userRouter=require("./routes/user.route");
 const movieRouter = require("./routes/movie.route");
 const { setupDefault } = require('./startUp/defaultdb');
-const cors = require('cors');
+// const cors = require('cors');
 const app=express();
-app.use(cors());
+// app.use(cors());
 
 // Curb Cores Error by adding a header here
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", '*');
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Credentials", true
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", '*');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Credentials", true
+  );
+  next();
+});
 
 
 app.use(express.json())
