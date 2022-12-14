b201d24fd20d97b9570ec25f8df8d47326063e4f
Sunil Mohan <sunilvm2017@email.iimcal.ac.in>
2022-12-14 19:26:01 +0530

    asdasd

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ const app=express();
 app.use(cors());
 
 // Curb Cores Error by adding a header here
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", '*');
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Credentials", true
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", '*');
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Credentials", true
+//   );
+//   next();
+// });
 
 
 app.use(express.json())
