feb5f789aa23172b02d6a032761f3ae0c646d08b
Sunil Mohan <sunilvm2017@email.iimcal.ac.in>
2022-12-14 18:35:00 +0530

    asdasd

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const movieRouter = require("./routes/movie.route");
 const { setupDefault } = require('./startUp/defaultdb');
 const cors = require('cors');
 const app=express();
-app.use(cors({origin:null}));
+// app.use(cors({origin:null}));
 
 // Curb Cores Error by adding a header here
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://eclectic-jelly-fdf977.netlify.app/");
+  res.setHeader("Access-Control-Allow-Origin", "https://eclectic-jelly-fdf977.netlify.app");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
