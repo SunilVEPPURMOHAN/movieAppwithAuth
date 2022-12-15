5c8d3022798a03bf36207fbbf3be6d023c9178f1
Sunil Mohan <sunilvm2017@email.iimcal.ac.in>
2022-12-15 19:19:02 +0530

    asdsad

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,7 +1,16 @@
 const Movie = require("../models/movie.model");
 
 module.exports.createMovies = async(req,res,next)=>{
-    const moviedb = req.body;
+    const {userId, userEmail} = req.user;
+    const { title, year, genres, imdbRating} = req.body;
+    const moviedb = [{title:title,year: year,genres:genres,imdbRating:imdbRating,
+    poster: "MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY500_CR0,0,337,500_AL_.jpg",
+    posterurl: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY500_CR0,0,337,500_AL_.jpg",
+    actors:["Unknown"],
+    createdBy:userId
+}];
+
+    if(genres.length === 0) return res.status(202).json({message:"You didn't add genre details"});
     // console.log(moviedb);
     Movie.create(moviedb,(err,docs)=>{
         if(err){
@@ -9,7 +18,7 @@ module.exports.createMovies = async(req,res,next)=>{
         res.status(202).json({message:'Check your input'});
 
     }else{
-        res.status(200).json({ message: 'Documents added successfully' });
+        res.status(200).json({ message: 'Document added successfully' });
     }
     });
 }
--- routes/movie.route.js
+++ routes/movie.route.js
@@ -1,10 +1,12 @@
 const express=require("express");
 const auth = require("../controllers/auth");
-const { findMovies, myMovies } = require("../controllers/movie.controller");
+const { findMovies, myMovies, createMovies } = require("../controllers/movie.controller");
 
 const router = express.Router();
 
 router.get("/home", auth, findMovies);
 router.get("/mymovies", auth, myMovies);
+
+router.post("/createmovies",auth,createMovies);
 
 module.exports = router
